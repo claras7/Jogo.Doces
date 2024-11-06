dc23d77529e4fc1a2f100696ca4baedbacc8ddf1
claras7 <145567064+claras7@users.noreply.github.com>
2024-11-06 19:15:36 -0300

    camera

---
+++ Jogo.doces/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+    
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = Player.transform.position + new Vector3(0,0,-10);
+        newPosition.y = 0.1f;
+        transform.position = newPosition;
+    }
+    
+}
