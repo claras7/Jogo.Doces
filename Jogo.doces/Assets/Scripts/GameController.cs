fb5349e74051057e76dfd15f5b48aa4db176ca0c
claras7 <145567064+claras7@users.noreply.github.com>
2024-11-07 08:24:08 -0300

    botao

---
+++ Jogo.doces/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOver;
+    public static GameController instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+    public void RestartGame(String lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
+    }
+}
